6d3001359ca8f52ed100b496627c28851f033073
Marcelo Vergara <60610755+marcelovergara-web@users.noreply.github.com>
2020-02-10 12:12:13 -0300

    teste

--- app/src/main/java/com/example/my_application/MainActivity.kt
+++ app/src/main/java/com/example/my_application/MainActivity.kt
@@ -10,5 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+    }//teste
 }
